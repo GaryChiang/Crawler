b5c011b595577f69dda18f5e058c301e59881957
Gary Chiang <i_mac0513@hotmail.com>
2018-08-19 02:49:10 +0800

    revise

--- stockcrawler/fetch_data.py
+++ stockcrawler/fetch_data.py
@@ -178,69 +178,10 @@ class FetchData:
     @staticmethod
     def __to_tw_time(ad_time):
         try:
-<<<<<<< Updated upstream
-            # 建立時間序列
-            self.__create_series()
-            # 當月及上個月必須重轉, 無論是否已經轉過
-            start = datetime.date.today()
-            current_month = str(start.year) + str(start.month).zfill(2)
-            last_month = self.__add_months(start, -1)
-            last_month = str(last_month.year) + str(last_month.month).zfill(2)
-
-            # # delete overlay part
-            # delete_duration = self.__add_months(start, -self.duration)
-
-            # query all need transfer companies
-            company = self.__session.query(Company.StockID).filter(Company.Type == '股票')
-
-            for item in company:
-                # init instance
-                stock = twstock.Stock(item.StockID, False)
-
-                # check log, if this period has been executed, pass this loop
-                log_history = self.__session.query(CrawlerLog.Param2).filter(
-                    CrawlerLog.FunName == 'fetch_history_stock_price',
-                    CrawlerLog.Type == 'info',
-                    CrawlerLog.Param2 != current_month,
-                    CrawlerLog.Param2 != last_month,
-                    CrawlerLog.Param1 == item.StockID).all()
-
-                log_history = [value for (value,) in log_history]
-
-                time_series = self.__session.query(TimeSeries.Series).filter(
-                    TimeSeries.Series.notin_(log_history)).all()
-
-                time_series = [value for (value,) in time_series]
-
-                for x in time_series:
-                    try:
-                        data = stock.fetch(int(x[0:4]), int(x[4:6]))
-                    except ValueError:
-                        continue
-
-                    time.sleep(2)
-                    for day in data:
-                        # delete all this stock history price
-                        self.__session.query(Price).filter(Price.Date == day.date,
-                                                           Price.StockID == item.StockID).delete()
-
-                        price = Price(UID=str(uuid.uuid4()), Date=day.date, StockID=item.StockID, Open=day.open,
-                                      Close=day.close, High=day.high, Low=day.low, Change=day.change,
-                                      Transaction=day.transaction, Capacity=day.capacity, Turnover=day.turnover,
-                                      CreateDt=datetime.datetime.now())
-                        self.__session.add(price)
-                    self.__log('fetch_history_stock_price', item.StockID, x, 'info')
-                    self.__session.commit()
-
-        except Exception as e:
-            self.__session.rollback()
-            self.__log_error('fetch_history_stock_price', str(e))
-=======
             tw_time = str(int(ad_time[0:4]) - 1911) + '/' + ad_time[4:6] + '/' + ad_time[6:8]
 
             return tw_time
         except Exception:
->>>>>>> Stashed changes
             raise
 
     def execute(self):
